5f2517060dca708a9c291d70a221c28ff5ad9fc4
Sam Delamere <sam.delamere@gmail.com>
2014-12-14 11:49:29 -0900

    Formatting, Text changes.

--- zork.py
+++ zork.py
@@ -7,46 +7,46 @@ class luke1:
     self.legs = legs
     self.color = color
         
-luke = luke1('Doctor who fanatic', 2, 'white')
-sam = luke1('Wizzard', 2, 'Perfectly tanned')
-print('Lets learn about two muggles named Luke and Sam! Press enter to continue.')
+luke = luke1('Doctor Who fanatic', 2, 'white')
+sam = luke1('Wizard', 2, 'Perfectly tanned')
+print('Let us learn about two muggles named Luke and Sam! Press enter to continue.')
 userName=raw_input();
-print "In this educational guide, you must put everything you type in\'\' and capitalize the first letter of everything.Have much fun!!!{}.".format(userName)
+print "In this educational guide, you must capitalize the first letter of phrase you enter. Have much fun!!!{}.".format(userName)
 sys.stdout.flush()
-time.sleep(6)
-print('Say \'Ok\' if you would like to continue')
-learn = input('Type here: ')
+time.sleep(3)
+print('Say Ok if you would like to continue')
+learn = raw_input('Type here: ')
 if learn == 'Ok':
   print('Alright then')
 else:
   print('Go away then...')
-print('I can tell you what species they are, how many legs the have, or what their nateraul skin color is!')
+print('I can tell you what species they are, how many legs they have, or what their natural skin color is!')
 info_input = [sam.species, sam.legs, sam.color, luke.species, luke.legs, luke.color]
-input_1 = input('Type \'Species\', \'Legs\', or \'Color\':')
+input_1 = raw_input('Type \'Species\', \'Legs\', or \'Color\':')
 if input_1 == 'Species':
-  print('do you want to know about Luke or Sam?')
-  input_1 = input('Type \'Luke\' or \'Sam\' to become an educated being: ')
+  print('Do you want to know about Luke or Sam?')
+  input_1 = raw_input('Type Luke or Sam to become an educated being: ')
   if input_1 == 'Sam':
     print(sam.species)
   if input_1 == 'Luke':
     print(luke.species)
 elif input_1 == 'Legs':
-  print('do you want to know about luke or Sam?')
-  input_1 = input('Type \'Luke\' or \'Sam\' to become an educated being: ')
+  print('Do you want to know about Luke or Sam?')
+  input_1 = raw_input('Type Luke or Sam to become an educated being: ')
   if input_1 == 'Sam':
     print(sam.legs)
   if input_1 == 'Luke':
     print(luke.legs)
 elif input_1 == 'Color':
-  print('do you want to know about luke or Sam?')
-  input_1 = input('Type \'Luke\' or \'Sam\' to become an educated being: ')
+  print('Do you want to know about Luke or Sam?')
+  input_1 = raw_input('Type Luke or Sam to become an educated being: ')
   if input_1 == 'Sam':
     print(sam.color)
   if input_1 == 'Luke':
     print(luke.color)
-print("You know what? I like you. You no longer have to type \'\' or capitalize the first letter anymore. Your free! There is only one problem, I dont remember who you learned about.")
-print('Sorry my memory is aweful sometimes.')
-print('Can you type the name of the person who you learned about for me. Infact if you do i\'ll make a text based adventure game just for you! Enter who you learned about here: ')
+print("You know what? I like you. Your free! There is only one problem, I do not remember who you learned about.")
+print('Sorry my memory is awful sometimes.')
+print('Can you type the name of the person who you learned about for me? In fact if you do, I\'ll make a text-based adventure game just for you! Put yourself in their shoes as they go on this epic quest. Whom did you learn about? Choose Luke or Sam.')
 name = raw_input()
 print(' ')
 print "Welcome to Zork 2.0, %s. Your adventure starts here." % (name)
@@ -62,14 +62,14 @@ shop = raw_input('your choice of establishment:')
 if shop == "Ye Olde Hat Shoppe":
   print("You enter the shop, and, as expected, it is filled with hats of all kinds. There is a tall, thin, pale man behind the counter who asks, \"Would you like to buy a hat?\"")
   print
-  print("Wizard hat")
+  print("Wizard's hat")
   print("Knight's helmet")
   print("Fez")
   print("No thanks")
   print(' ')
-  hat = raw_input('your choice of hat:')
-  if hat == "Wizard hat":
-    hat = "wizard"
+  hat = raw_input('Your choice of hat:')
+  if hat == "Wizard's hat":
+    hat = "Wizard"
     print("\"Excellent choice!\"")
   elif hat == "Knight's helmet":
     hat = "helmet"
@@ -78,7 +78,7 @@ if shop == "Ye Olde Hat Shoppe":
     hat = "cool"
     print("\"Excellent choice!\"")
   elif hat == "No thanks":
-    hat = "none"
+    hat = "None"
     print("\"Very well.\"")
   print("You return to the courtyard.")
 elif shop == "The Jolly Man's Cave":
@@ -96,7 +96,7 @@ elif shop == "The Jolly Man's Cave":
     print("\"Ahhhhh you choose pete.\"")
     print("The bony man walks into the circle and muters\"you didn't have to do this to yourself %s" % (name))
     print("The man threw a punch with suprising force.")
-    print("His giant knukles made contact with your face.")
+    print("His giant knuckles made contact with your face.")
     print("Stars are spinning around your head.")
     print(" ")
     print(" ")
